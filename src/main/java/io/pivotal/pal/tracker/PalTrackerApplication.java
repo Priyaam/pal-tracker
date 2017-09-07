f46154f5a073a79b6254669227ceeb55c1bd982e
Priyaam Das <priyaam.das@capgemini.com>
2017-09-07 08:25:15 +0530

    --updated- applications: - name: pal-tracker   path: build/libs/pal-tracker.jar   host: ps-pal-tracker-review   services:   - tracker-database

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,9 @@
 package io.pivotal.pal.tracker;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -12,7 +13,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
